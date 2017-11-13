5329a4b4507c8b565a092b156a1551e69b0e90ee
Edison Lee <edisonlee@edisonlee55.com>
2017-11-13 09:38:32 +0800

    Add LICENSE info to bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2017 MING-CHIEN LEE
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 //
 // This is main file containing code implementing the Express server and functionality for the Express Koguchi Chino Messenger bot.
 //
